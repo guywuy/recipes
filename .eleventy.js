032c2a6e96d6ad0e1944c6fec3f584269e9b0d81
Guy <gcschino@hotmail.com>
2020-08-07 23:29:24 +0100

    Remove blog, tags, etc, customise

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,6 @@
-const syntaxHighlightPlugin = require('@11ty/eleventy-plugin-syntaxhighlight')
 const htmlMinTransform = require('./utils/transforms/htmlmin.js')
 const contentParser = require('./utils/transforms/contentParser.js')
-const htmlDate = require('./utils/filters/htmlDate.js')
 const pwaPlugin = require('eleventy-plugin-pwa')
-const date = require('./utils/filters/date.js')
 const fs = require('fs')
 
 /**
@@ -37,11 +34,8 @@ module.exports = function (eleventyConfig) {
    *
    * @link https://www.11ty.io/docs/filters/
    */
-  // human friendly date format
-  eleventyConfig.addFilter('dateFilter', date)
-  // robot friendly date format for crawlers
-  eleventyConfig.addFilter('htmlDate', htmlDate)
 
+   
   /**
    * Add Transforms
    *
@@ -56,29 +50,10 @@ module.exports = function (eleventyConfig) {
 
   /**
    * Add Plugins
-   * @link https://github.com/11ty/eleventy-plugin-syntaxhighlight
    * @link https://github.com/okitavera/eleventy-plugin-pwa
    */
-  eleventyConfig.addPlugin(syntaxHighlightPlugin)
   eleventyConfig.addPlugin(pwaPlugin)
 
-  /**
-   * Create custom data collections
-   * for blog and feed
-   * Code from https://github.com/hankchizljaw/hylia
-   */
-  // Blog posts collection
-  const now = new Date()
-  const livePosts = post => post.date <= now && !post.data.draft
-  eleventyConfig.addCollection('posts', collection => {
-    return [
-      ...collection
-      .getFilteredByGlob(
-        `./${siteConfig.paths.src}/${siteConfig.paths.blogdir}/**/*`
-      )
-      .filter(livePosts),
-    ]
-  })
 
   /**
    * Override BrowserSync Server options
