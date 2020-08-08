02ce6e8acb3e49ef1f4b4b84d1babdb7f90757f9
Guy <gcschino@hotmail.com>
2020-08-08 12:00:11 +0100

    Add permalinks to headings

--- utils/transforms/contentParser.js
+++ utils/transforms/contentParser.js
@@ -64,7 +64,7 @@ module.exports = function(value, outputPath) {
      */
     const articleHeadings = [
       ...document.querySelectorAll(
-        'article h2, article h3, article h4, article h5, article h6'
+        'main h1, main h2, main h3, main h4, main h5, main h6'
       ),
     ]
     if (articleHeadings.length) {
